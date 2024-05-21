0e3e581e588607ff8eadbda95b3ab2d7850e58d1
Gonzalo Jimenez <65154469+Gonzo909@users.noreply.github.com>
2024-05-20 18:38:46 -0600

    integrated tailwind and added some components

---
+++ src/components/Auth.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+
+const Auth = ({ onLogin }) => {
+    const handleSpotifyLogin = () => {
+        const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+        const redirectUri = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+        const scopes = [
+            'user-read-private',
+            'user-read-email',
+            'playlist-read-private',
+        ];
+        const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes.join('%20')}&redirect_uri=${redirectUri}`;
+        window.location = authUrl;
+    };
+
+    React.useEffect(() => {
+        const hash = window.location.hash.substring(1);
+        const params = new URLSearchParams(hash);
+        const accessToken = params.get('access_token');
+
+        if (accessToken) {
+            spotifyApi.setAccessToken(accessToken);
+            onLogin(accessToken);
+        }
+    }, [onLogin]);
+
+    return ( 
+        <div>
+            <button onClick={handleSpotifyLogin}>Login with Spotify</button>
+        </div>
+    )
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import Auth from './Auth';
+import PlaylistCreator from './PlaylistCreator';
+import PlaylistFetcher from './PlaylistFetcher';
